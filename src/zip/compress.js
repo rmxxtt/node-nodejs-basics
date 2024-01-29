4fa9f1ca9fd40bc76bc6260fa96369c452b4ffd1
rmxxtt <62797019+rmxxtt@users.noreply.github.com>
2024-01-30 00:54:44 +0700

    zip compress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,10 @@
+import {createGzip} from 'node:zlib'
+import {createReadStream, createWriteStream} from "node:fs";
+
 const compress = async () => {
-    // Write your code here 
+    const source = './src/zip/files/fileToCompress.txt'
+    const destination ='./src/zip/files/archive.gz'
+    createReadStream(source).pipe(createGzip()).pipe(createWriteStream(destination))
 };
 
 await compress();
