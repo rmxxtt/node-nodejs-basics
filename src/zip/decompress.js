7739193e2583900872a9c949021dc88acd0d8274
rmxxtt <62797019+rmxxtt@users.noreply.github.com>
2024-01-30 00:59:52 +0700

    zip decompress

--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,10 @@
+import {createReadStream, createWriteStream} from "node:fs";
+import {createUnzip} from "node:zlib";
+
 const decompress = async () => {
-    // Write your code here 
+    const source = './src/zip/files/archive.gz'
+    const destination = './src/zip/files/fileToCompress.txt'
+    createReadStream(source).pipe(createUnzip()).pipe(createWriteStream(destination))
 };
 
 await decompress();
