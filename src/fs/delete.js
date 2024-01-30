4818e3a768655f91faf9ec637d073741a41c9be1
rmxxtt <62797019+rmxxtt@users.noreply.github.com>
2024-01-30 07:43:48 +0700

    fs delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,10 @@
+import { unlink } from 'node:fs';
+
 const remove = async () => {
-    // Write your code here 
+    unlink('./src/fs/files/fileToRemove.txt', err => {
+        if (err.code === 'ENOENT') throw new Error('FS operation failed');
+        console.log('File deleted');
+    })
 };
 
 await remove();
