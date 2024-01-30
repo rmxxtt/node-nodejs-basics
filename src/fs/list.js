83fe47a8e125a1911c37ae0afb05cac46b8b85d1
rmxxtt <62797019+rmxxtt@users.noreply.github.com>
2024-01-30 07:52:19 +0700

    fs list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,10 @@
+import {readdir} from 'node:fs'
+
 const list = async () => {
-    // Write your code here 
+    readdir('./src/fs', (err, files) =>{
+        if (err?.code === 'ENOENT') throw new Error('FS operation failed');
+        console.log(files);
+    });
 };
 
 await list();
